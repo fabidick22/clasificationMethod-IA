d44d2bbf497b11d6cb86417bd7cfcad8878bb189
Dickson <fabidick22@hotmail.es>
2018-07-22 22:50:40 -0500

    opciones para redes neuronales

--- classifiersPy.py
+++ classifiersPy.py
@@ -4,6 +4,7 @@
 import sys
 import argparse
 import logging
+from neural_network.neural_network import NeuralNet
 
 
 module = sys.modules['__main__'].__file__
@@ -20,11 +21,30 @@ def parse_command_line(argv):
     parser.add_argument("-v", "--verbose", dest="verbose_count",
                         action="count", default=0,
                         help="Aumenta la verbosidad del registro para cada ocurrencia.")
+    parser.add_argument("-nN", "--neuralNet",
+                        help="Usar red neuronal.")
+    parser.add_argument("-bN", "--bayesNet",
+                        help="Usar red bayesiana.")
     parser.add_argument('-o', metavar="output",
-                        type=argparse.FileType('w'), default=sys.stdout,
+                        type=argparse.FileType('r'), default=sys.stdout,
                         help="Redirigir la salida a un archivo")
+    parser.add_argument('-f', metavar="file",
+                        type=argparse.FileType('w'), default=sys.stdout,
+                        help="Archivo con datos de entrada (csv)")
+    parser.add_argument('-e', metavar="epoch",
+                        type=int, default=0,
+                        help="Epoch for Neural Network")
+    parser.add_argument('-nI', metavar="n_input",
+                        type=int, default=0,
+                        help="n_input")
+    parser.add_argument('-nH', metavar="n_hidden",
+                        type=int, default=0,
+                        help="n_hidden")
+    parser.add_argument('-nO', metavar="n_output",
+                        type=int, default=0,
+                        help="n_output")
     parser.add_argument('input', metavar="input", nargs='+', help="Cualquier entrada...")
-    arguments = parser.parse_args(argv[1:])
+    arguments = parser.parse_args(argv[0:])
 
     log.setLevel(max(3 - arguments.verbose_count, 0) * 10)
     return arguments
@@ -35,6 +55,8 @@ def main():
                         format='%(name)s (%(levelname)s): %(message)s')
     try:
         arguments = parse_command_line(sys.argv)
+        NeuralNet()
+        print(arguments.f)
         # Do something with arguments.
     except KeyboardInterrupt:
         log.error('Programa interrumpido!')
--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -1,24 +1,24 @@
-import pandas
 import tensorflow as tf
+import pandas as pd
+import numpy as np
+
 
 """
 reference:
 https://github.com/aymericdamien/TensorFlow-Examples/blob/master/notebooks/3_NeuralNetworks/neural_network_raw.ipynb
 https://becominghuman.ai/creating-your-own-neural-network-using-tensorflow-fa8ca7cc4d0e
 https://github.com/michaelwayman/python-ann/blob/master/neural_network/neural_network.py
+
+real
+https://github.com/nikhilroxtomar/Iris-Data-Set-Classification-using-TensorFlow-MLP/blob/master/iris.py
 """
 
 
 class NeuralNet(object):
 
-    def __init__(self, num_entradas, num_ocultos, num_salidas, num_classes):
-        """
+    def __init__(self, file, num_entradas, num_ocultos, num_salidas, num_classes):
 
-        :param num_entradas:
-        :param num_ocultos:
-        :param num_salidas:
-        :param num_classes:
-        """
+        self.data_file = file
         self.n_input = num_entradas
         self.n_hidden = num_ocultos
         self.n_output = num_salidas
