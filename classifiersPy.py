893e6d997a86e4d194da3a35a86dc213d3c8c73e
Dickson <fabidick22@hotmail.es>
2018-07-24 18:19:26 -0500

    conectaqdo el main a la clases de neural network

--- classifiersPy.py
+++ classifiersPy.py
@@ -1,16 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # encoding: utf-8
 
 import sys
 import argparse
 import logging
 from neural_network.neural_network import NeuralNet
+from bayes_net.bayes_net import BayesNet
 
 
 
 module = sys.modules['__main__'].__file__
 log = logging.getLogger(module)
 
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 def parse_command_line(argv):
     """Parse command line argument. See -h option
@@ -22,30 +30,33 @@ def parse_command_line(argv):
     parser.add_argument("-v", "--verbose", dest="verbose_count",
                         action="count", default=0,
                         help="Aumenta la verbosidad del registro para cada ocurrencia.")
+
     parser.add_argument("-nN", "--neuralNet",
+                        type=bool, default=False,
                         help="Usar red neuronal.")
+
     parser.add_argument("-bN", "--bayesNet",
+                        type=bool, default=False,
                         help="Usar red bayesiana.")
+
     parser.add_argument('-o', metavar="output",
                         type=argparse.FileType('r'), default=sys.stdout,
                         help="Redirigir la salida a un archivo")
-    parser.add_argument('-f', metavar="file",
-                        type=argparse.FileType('w'), default=sys.stdout,
+
+    parser.add_argument('-f', metavar="file", type=argparse.FileType('w'), default=sys.stdout,
                         help="Archivo con datos de entrada (csv)")
-    parser.add_argument('-e', metavar="epoch",
-                        type=int, default=0,
-                        help="Epoch for Neural Network")
-    parser.add_argument('-nI', metavar="n_input",
-                        type=int, default=0,
-                        help="n_input")
-    parser.add_argument('-nH', metavar="n_hidden",
-                        type=int, default=0,
-                        help="n_hidden")
-    parser.add_argument('-nO', metavar="n_output",
-                        type=int, default=0,
-                        help="n_output")
-    parser.add_argument('input', metavar="input", nargs='+', help="Cualquier entrada...")
-    arguments = parser.parse_args(argv[0:])
+
+    parser.add_argument('-e', metavar="epoch", type=int, default=0, help="Epoch for Neural Network")
+
+    parser.add_argument('-nI', metavar="n_input", type=int, default=0, help="n_input")
+
+    parser.add_argument('-nH', metavar="n_hidden", type=str, default=0, help="n_hidden")
+
+    parser.add_argument('-nO', metavar="n_output", type=int, default=0, help="n_output")
+
+    parser.add_argument('-nC', metavar="n_class", type=int, help="Numero de clases reales")
+
+    arguments = parser.parse_args(argv[1:])
 
     log.setLevel(max(3 - arguments.verbose_count, 0) * 10)
     return arguments
@@ -56,8 +67,19 @@ def main():
                         format='%(name)s (%(levelname)s): %(message)s')
     try:
         arguments = parse_command_line(sys.argv)
-        NeuralNet()
-        print(arguments.f)
+        if arguments.neuralNet:
+            if arguments.nH:
+                modelo=map(int, str(arguments.nH).split(","))
+                n_n = NeuralNet(arguments.f, arguments.nI, modelo, arguments.nO, arguments.nC, arguments.e)
+                log.info("info: objeto creado")
+                n_n.runClasificador()
+            else:
+                log.error("Se debe enviar el atributo -nH para crear el modelo")
+        elif arguments.bayesNet:
+            b_n=BayesNet()
+        else:
+            log.error("Ningun metodo de clasificación seleccinado")
+
         # Do something with arguments.
     except KeyboardInterrupt:
         log.error('Programa interrumpido!')
--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -1,10 +1,12 @@
+# encoding: utf-8
+
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 from sklearn import datasets
 from sklearn import metrics
 from sklearn import model_selection
-
+from sklearn import preprocessing
 
 """
 reference:
@@ -14,6 +16,9 @@ https://github.com/michaelwayman/python-ann/blob/master/neural_network/neural_ne
 
 real
 https://github.com/nikhilroxtomar/Iris-Data-Set-Classification-using-TensorFlow-MLP/blob/master/iris.py
+https://github.com/chadlimedamine/kdd-cup-99-Analysis-machine-learning-python/blob/master/kdd_binary_classification_ANN.py
+https://github.com/Belval/ML-IDS
+https://github.com/khakhulin/IntrusionDetection
 """
 
 
@@ -21,13 +26,12 @@ class NeuralNet(object):
 
     def __init__(self, file, num_entradas, num_ocultos, num_salidas, num_classes, epoch=200):
 
-        res = datasets.fetch_kddcup99()
-        self.data_file = res
+        self.data_file = file
         self.n_input = num_entradas
         self.n_hidden = num_ocultos
         self.n_output = num_salidas
         self.n_class = num_classes
-        self.epoch = epoch
+        self.n_epoch = epoch
         self.dataSetBreak = {"dataTrain": None,
                              "dataTest": None,
                              "classTrain": None,
@@ -39,7 +43,7 @@ class NeuralNet(object):
         :return:
         """
         classifier = classifierModel
-        classifier.fit(self.dataSetBreak["dataTrain"], self.dataSetBreak["classTrain"], steps=self.epoch)
+        classifier.fit(self.dataSetBreak["dataTrain"], self.dataSetBreak["classTrain"], steps=self.n_epoch)
 
         # usamos el modelo entrenado para realizar predicciones con los datos de test
         predictions = list(classifier.predict(self.dataSetBreak["dataTest"], as_iterable=True))
@@ -50,22 +54,33 @@ class NeuralNet(object):
         self.break_datadet()
         if self.n_class is None:
             self.n_class = len(set(self.data_file.target))
-        f_columnas = tf.contrib.learn.infer_real_valued_columns_from_input(self.dataSetBreak["dataTrain"])
+
+        # f = [tf.feature_column.numeric_column("x", shape=[4])]
+        # f_columnas = tf.contrib.learn.infer_real_valued_columns_from_input(self.dataSetBreak["dataTrain"])
+        feature_columns = [tf.feature_column.numeric_column("x", shape=[4])]
         # hidden_units= [10, 20, 10] se va a crear un modelo de 3 capas con 10, 20 y 10 unidades respectivamente
-        return tf.contrib.learn.DNNClassifier(feature_columns=f_columnas, hidden_units=self.n_hidden,
-                                                    n_classes=self.n_class)
+        clasi = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns, hidden_units=self.n_hidden,
+                                               n_classes=self.n_class)
+        return clasi
 
     def break_datadet(self):
         target = self.data_file.target
+
+        # target = le.fit_transform(target)
         data = self.data_file.data
-        x_train, x_test, y_train, y_test = model_selection\
-            .train_test_split(data, target, test_size=0.2, random_state=42)
+        x_train, x_test, y_train, y_test = model_selection.train_test_split(data, target,
+                                                                            test_size=0.2, random_state=42)
         self.dataSetBreak["dataTrain"]=x_train
         self.dataSetBreak["dataTest"]=x_test
         self.dataSetBreak["classTrain"]=y_train
         self.dataSetBreak["classTest"]=y_test
 
     def runClasificador(self):
+        self.data_file = datasets.fetch_kddcup99()
+
+        le = preprocessing.LabelEncoder()
+        self.data_file.target = le.fit_transform(self.data_file.target)
+
         clasificador = self.crete_model_net()
         predictions = self.entrenar(clasificador)
 
