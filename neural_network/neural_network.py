6ba8bdc5b7d6f874dbc39eee0c17ac156718e849
Dickson <fabidick22@hotmail.es>
2018-07-24 09:58:58 -0500

    clase para clasificar datos con redes neuronales y agregar iptyhon a las dependencias del proyecto

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 from sklearn import datasets
+from sklearn import metrics
 from sklearn import model_selection
 
 
@@ -18,7 +19,7 @@ https://github.com/nikhilroxtomar/Iris-Data-Set-Classification-using-TensorFlow-
 
 class NeuralNet(object):
 
-    def __init__(self, file, num_entradas, num_ocultos, num_salidas, num_classes):
+    def __init__(self, file, num_entradas, num_ocultos, num_salidas, num_classes, epoch=200):
 
         res = datasets.fetch_kddcup99()
         self.data_file = res
@@ -26,20 +27,48 @@ class NeuralNet(object):
         self.n_hidden = num_ocultos
         self.n_output = num_salidas
         self.n_class = num_classes
+        self.epoch = epoch
+        self.dataSetBreak = {"dataTrain": None,
+                             "dataTest": None,
+                             "classTrain": None,
+                             "classTest": None}
+
+    def entrenar(self, classifierModel):
+        """
+        entrenamos la red neuronal con los datos de entrenamiento
+        :return:
+        """
+        classifier = classifierModel
+        classifier.fit(self.dataSetBreak["dataTrain"], self.dataSetBreak["classTrain"], steps=self.epoch)
+
+        # usamos el modelo entrenado para realizar predicciones con los datos de test
+        predictions = list(classifier.predict(self.dataSetBreak["dataTest"], as_iterable=True))
+        return predictions
 
 
-    def entrenar(self):
-        pass
+    def crete_model_net(self):
+        self.break_datadet()
+        if self.n_class is None:
+            self.n_class = len(set(self.data_file.target))
+        f_columnas = tf.contrib.learn.infer_real_valued_columns_from_input(self.dataSetBreak["dataTrain"])
+        # hidden_units= [10, 20, 10] se va a crear un modelo de 3 capas con 10, 20 y 10 unidades respectivamente
+        return tf.contrib.learn.DNNClassifier(feature_columns=f_columnas, hidden_units=self.n_hidden,
+                                                    n_classes=self.n_class)
 
-    def breakDataSet(self):
-        target= self.data_file.target
-        data= self.data_file.data
-        dataSetBreak={"dataTrain":None,"dataTest":None,"classTrain":None,"classTest":None}
+    def break_datadet(self):
+        target = self.data_file.target
+        data = self.data_file.data
         x_train, x_test, y_train, y_test = model_selection\
             .train_test_split(data, target, test_size=0.2, random_state=42)
-        dataSetBreak["dataTrain"]=x_train
-        dataSetBreak["dataTest"]=x_test
-        dataSetBreak["classTrain"]=y_train
-        dataSetBreak["classTest"]=y_test
-        return dataSetBreak
+        self.dataSetBreak["dataTrain"]=x_train
+        self.dataSetBreak["dataTest"]=x_test
+        self.dataSetBreak["classTrain"]=y_train
+        self.dataSetBreak["classTest"]=y_test
 
+    def runClasificador(self):
+        clasificador = self.crete_model_net()
+        predictions = self.entrenar(clasificador)
+
+        # comprobamos como de buenas han sido las predicciones y calculamos la precisión de nuestro modelo
+        score = metrics.accuracy_score(self.dataSetBreak["classTest"], predictions)
+        print('Accuracy: {0:f}'.format(score))
